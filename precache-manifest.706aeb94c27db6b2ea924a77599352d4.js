df6759beb4e09258a16de7353614fdf93d8f6dbf
Harry Cheng <chengyuhui1@gmail.com>
2020-08-03 22:34:24 +0800

    deploy

---
+++ precache-manifest.706aeb94c27db6b2ea924a77599352d4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66525e457279c73f3768ece1ad37534",
+    "url": "/audio.json"
+  },
+  {
+    "revision": "6f2329ea1fbaec538a4ec48211fad9a1",
+    "url": "/audio/2020-06-17 21-49-32_01.mp3"
+  },
+  {
+    "revision": "88397808fea5f10fb65627d59b6e0c3f",
+    "url": "/audio/BV1DE411T7xm-00_00_02-0_04.000.mp3"
+  },
+  {
+    "revision": "f2d2eafb17f5bb5c56e507994c824db3",
+    "url": "/audio/BV1DE411T7xm-00_00_48-0_05.500.mp3"
+  },
+  {
+    "revision": "79f4d77b0241a06045dc5165189cc349",
+    "url": "/audio/BV1DE411T7xm-00_01_37.500-0_04.000.mp3"
+  },
+  {
+    "revision": "44c4313d2d6b890db8c398bbe346cdcf",
+    "url": "/audio/BV1DE411T7xm-00_02_43.000-0_01.500.mp3"
+  },
+  {
+    "revision": "b4fd2c62d1c1dff07b0c39a7e9879f36",
+    "url": "/audio/BV1Fg4y1z77s-1_58_30.000-0_5.000.mp3"
+  },
+  {
+    "revision": "993bdcc2eeff66ea2bd69ea443965781",
+    "url": "/audio/BV1sC4y1b7Fk-00_05.00-0_04.000.mp3"
+  },
+  {
+    "revision": "609eb874a5c956772531aa95e0c1df01",
+    "url": "/audio/BV1sC4y1b7Fk-00_12.00-0_02.000.mp3"
+  },
+  {
+    "revision": "1cecf7ee7f8a0a8eface5aab943deb83",
+    "url": "/audio/BV1xE411V7FH-00_00.00-0_05.000.mp3"
+  },
+  {
+    "revision": "cfec3f88eb4fb0575399ce9b750a8cc5",
+    "url": "/audio/BV1xE411V7FH-00_05.500-0_03.000.mp3"
+  },
+  {
+    "revision": "5be916ba34b1b5ad9ab72152cdeb5d81",
+    "url": "/bili_ts.user.js"
+  },
+  {
+    "revision": "23c6c5e6be39d3b7549d",
+    "url": "/css/app.8ebc2c30.css"
+  },
+  {
+    "revision": "266ab37511b9277a42aa67bd65951283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23c6c5e6be39d3b7549d",
+    "url": "/js/app.aa76e5ba.js"
+  },
+  {
+    "revision": "39e6738e486014987453",
+    "url": "/js/chunk-vendors.8785194c.js"
+  },
+  {
+    "revision": "78888d66c19a6168ddff498a821841a0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "07e3d0814e1272c7c462c45aa766287b",
+    "url": "/particles.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
